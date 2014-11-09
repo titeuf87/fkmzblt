af76f2f97e98e04208dff3aa4b2c9b978321676f
Simon Vermeersch <simonvermeersch@gmail.com>
2014-11-09 13:16:06 +0100

    Remove unused code

--- server.py
+++ server.py
@@ -85,38 +85,8 @@ def downloader_connected_to_proxy(original_data, sharerid, local_reader, local_w
     yield from p.run()
     local_writer.close()
     print("all done")
-    return
-
-    local_read = asyncio.async(local_reader.read(1024))
-    remote_read = asyncio.async(q.get())
-
-    while local_read or remote_read:
-        waits = [t for t in (local_read, remote_read) if t]
-        done, pending = yield from asyncio.wait(waits,
-            return_when=asyncio.FIRST_COMPLETED)
-
-        for t in done:
-            data = t.result()
-
-            if t == local_read:
-                if data:
-                    local_read = asyncio.async(local_reader.read(1024))
-                    remote_writer.write(protocol.encode(downloaderid, protocol.PACKET, data))
-                else:
-                    local_read = None
-                    #remote_writer.close()
-
-            elif t == remote_read:
-                if data:
-                    remote_read = asyncio.async(q.get())
-                    local_writer.write(data)
-                else:
-                    remote_read = None
-                    local_writer.close()
 
     del downloaders[downloaderid]
-#    while True:
-#        data = yield from local_reader.read(1024)
 
 sharers = {}
 downloaders = {}
